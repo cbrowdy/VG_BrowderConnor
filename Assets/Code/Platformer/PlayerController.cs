790c37862ec0bb884555f3390111805d13cb29df
Connor Browder <connorbrowder@wustl.edu>
2023-03-07 01:21:16 -0600

    Q4

--- Assets/Code/Platformer/PlayerController.cs
+++ Assets/Code/Platformer/PlayerController.cs
@@ -15,6 +15,8 @@ namespace Platformer
         Rigidbody2D _rigidbody2D;
         public Transform aimPivot;
         public GameObject projectilePrefab;
+        SpriteRenderer sprite;
+        Animator animator;
 
         public int jumpsLeft;
 
@@ -22,7 +24,7 @@ namespace Platformer
         {
             if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
             {
-                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 0.7f);
+                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 0.85f);
                 for (int i = 0; i < hits.Length; i++)
                 {
                     RaycastHit2D hit = hits[i];
@@ -37,6 +39,21 @@ namespace Platformer
         void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            sprite = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+        }
+
+        private void FixedUpdate()
+        {
+            animator.SetFloat("Speed", _rigidbody2D.velocity.magnitude);
+            if (_rigidbody2D.velocity.magnitude > 0)
+            {
+                animator.speed = _rigidbody2D.velocity.magnitude / 3f;
+            }
+            else
+            {
+                animator.speed = 1f;
+            }
         }
 
         // Update is called once per frame
@@ -44,11 +61,13 @@ namespace Platformer
         {
             if (Input.GetKey(KeyCode.A))
             {
-               _rigidbody2D.AddForce(Vector2.left*18f*Time.deltaTime,ForceMode2D.Impulse); 
+               _rigidbody2D.AddForce(Vector2.left*18f*Time.deltaTime,ForceMode2D.Impulse);
+               sprite.flipX = true;
             }
             if (Input.GetKey(KeyCode.D))
             {
                 _rigidbody2D.AddForce(Vector2.right*18f*Time.deltaTime,ForceMode2D.Impulse); 
+                sprite.flipX = false;
             }
 
             if (Input.GetKeyDown(KeyCode.Space))
@@ -59,7 +78,8 @@ namespace Platformer
                     _rigidbody2D.AddForce(Vector2.up*15f,ForceMode2D.Impulse);
                 }
             }
-
+            animator.SetInteger("JumpsLeft",jumpsLeft);
+            
             Vector3 mousePosition = Input.mousePosition;
             Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePosition);
             Vector3 directionFromPlayerToMouse = mousePositionInWorld - transform.position;
